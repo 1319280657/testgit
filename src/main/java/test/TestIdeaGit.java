e46205a22c4ecf6562b959059fa96c035fdbe045
renchao <1319280657@qq.com>
2018-10-16 20:11:05 +0800

    新增了一个测试的类

---
+++ src/main/java/test/TestIdeaGit.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @program: ceshi
+ * @description: ${description}
+ * @author: RenChao
+ * @create: 2018-10-16 20:08
+ **/
+public class TestIdeaGit {
+
+    public void show_Message(){
+        System.out.println("测试是否可以上传成功！");
+    }
+}
